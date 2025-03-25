6f3d52addd8b8b6e72bb6c9e1782738163b92ef8
yjgaia <yjdev88@gmail.com>
2025-03-25 14:57:35 +0900

    Add fetchCollectionStats method to TheGodsDashboard for fetching and displaying collection statistics

--- lib/TheGodsDashboard.d.ts
+++ lib/TheGodsDashboard.d.ts
@@ -1,5 +1,6 @@
 import { DomNode } from "@common-module/app";
 export default class TheGodsDashboard extends DomNode {
     constructor();
+    private fetchCollectionStats;
 }
 //# sourceMappingURL=TheGodsDashboard.d.ts.map
